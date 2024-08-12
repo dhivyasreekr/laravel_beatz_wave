c4a3deff2f57a7afad09b77efe677b23e534193b
dhivyasreekr <dhivyasreekr@gmail.com>
2024-08-12 18:39:21 +0530

    Create SongSeed.php
    php artisan make:seed SongSeed

---
+++ database/seeders/SongSeed.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Genre;
+use App\Models\Song;
+
+class SongSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $genres = Genre::all();
+
+        $songs = [
+            [
+                'name' => 'Badass',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Bloody Sweet',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Entha Thottavai Eduthalum',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Kaaki Sattai Theme ',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Right Polam Right',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Ulagam Oruvanukka',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Ungalukku India Gate',
+                'genre_id' => $genres->where('name', 'Rock')->first()->id
+            ],
+            [
+                'name' => 'Osaka Osaka',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Un Kaigal Korthu',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Kadhal Kan Kattuthe',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Kannazhaga',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Right Polam Right',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Minnalai Pidithu',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Melliname Melliname',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+            [
+                'name' => 'Innisai Paadi Varum',
+                'genre_id' => $genres->where('name', 'Classical')->first()->id
+            ],
+        ];
+
+        foreach ($songs as $row) 
+        {
+            Song::create($row);
+        }
+
+
+    }
+}
