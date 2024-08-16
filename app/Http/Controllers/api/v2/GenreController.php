be72671df774bad3d78a4b97c0722e0c47c7a1f4
dhivyasreekr <dhivyasreekr@gmail.com>
2024-08-16 12:42:08 +0530

    Create GenreController.php
    php artisan make:controller api/v2/GenreController --api --model=Genre

---
+++ app/Http/Controllers/api/v2/GenreController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\api\v2;
+
+use App\Http\Controllers\Controller;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $genres = Genre::all();
+
+        return response()->json(['data' => $genres], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Genre $genre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Genre $genre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Genre $genre)
+    {
+        //
+    }
+}
