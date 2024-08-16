39ac5016318e0578ee79c08d60b03f18affdfc94
dhivyasreekr <dhivyasreekr@gmail.com>
2024-08-16 18:52:59 +0530

    Create SongHistoryController.php
    php artisan make:controller api/v2/SongHistoryController --api --model=SongHistory

---
+++ app/Http/Controllers/api/v2/SongHistoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\api\v2;
+
+use App\Http\Controllers\Controller;
+use App\Models\SongHistory;
+use Illuminate\Http\Request;
+
+class SongHistoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SongHistory $songHistory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SongHistory $songHistory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SongHistory $songHistory)
+    {
+        //
+    }
+}
