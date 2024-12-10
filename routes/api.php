3f9d15a37816ff1500395e69fd9e5afeb5dd6841
dhivyasreekr <dhivyasreekr@gmail.com>
2024-12-10 16:32:50 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -31,9 +31,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
 use App\Http\Controllers\api\v2\GenreController;
 use App\Http\Controllers\api\v2\BrandController;
+use App\Http\Controllers\api\v2\SongController;
 
 
 Route::get('/genres', [GenreController::class, 'index']);
 Route::get('/brands', [BrandController::class, 'index']);
+Route::get('/songs', [SongController::class, 'index']);
 
 
