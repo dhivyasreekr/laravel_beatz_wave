3fab27efd3b9d2d8fda87abf79538c9a237fae31
dhivyasreekr <dhivyasreekr@gmail.com>
2024-12-10 16:32:47 +0530

    Update SongController.php

--- app/Http/Controllers/api/v2/SongController.php
+++ app/Http/Controllers/api/v2/SongController.php
@@ -12,6 +12,38 @@ use App\Models\SongHistory;
 
 class SongController extends Controller
 {
+    public function index(Request $request)
+    {
+        if($request->has('genre_id'))
+        {
+            $song=Song::where('genre_id',$request->input('genre_id'))
+                            ->orderBy('name', 'asc')
+                            ->get();
+        }
+        elseif ($request->has('brand_id'))
+        {
+            $song=Song::where('brand_id',$request->input('brand_id'))
+                            ->orderBy('name', 'asc')
+                            ->get();
+        }
+        else
+        {
+            $songs = Song::orderby('name', 'asc')->get();
+        }
+
+        $transformedSongs =$songs->map(function($row) {
+            return [
+                'id'=>$row->id,
+                'name'=>$row->name,
+                'image_path'=>$row->GetImagePath(),
+                'song_path'=>$row->GetSongPath()
+            ];
+        });
+
+        return response()->json(['data' => $transformedSongs], 200);
+    }
+
+
     /**
      * Get a list of movies by genre.
      *
