46663c6d29d393c96b04c7aca464dde3c696c640
dhivyasreekr <dhivyasreekr@gmail.com>
2024-12-10 16:32:40 +0530

    Update Song.php

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -5,18 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\Storage;
+
 class Song extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'genre_id',
-        'name'
+        'name',
+        'image_path',
+        'song_path'
         
     ];
 
     public function genre()
     {
         return $this->belongsTo(Genre::class);
+    }
+    public function GetImagePath()
+    {
+        return env('DOMAIN_URL') . Storage::url($this->image_path);
+    }
+    public function GetSongpath()
+    {
+        return env('DOMAIN_URL') . Storage::url($this->song_path);
     }
 }
